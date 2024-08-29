b876fdf84d4f5efbe74d92931069c2ff9142b6a0
Semenov Vasily <vvs1771@gmail.com>
2024-08-30 00:19:22 +0300

    changing the function recipes

--- recipes/calculator/views.py
+++ recipes/calculator/views.py
@@ -25,7 +25,7 @@ def home(request):
     # Получаем номер страницы блюда (по-умолчанию 1)
     num = int(request.GET.get('page', 1))
     # Используем пагинацию для вывода 1 блюда из словаря DATA на страницу
-    paginator = Paginator([{key: value} for key, value in DATA.items()], 1)
+    paginator = Paginator([{key: value} for key, value in DATA.items()], 2)
     # Формируем данные для отправки в шаблон и вывода на страницу
     page = paginator.get_page(num)
     data = {'title': 'Главная страница',
@@ -38,13 +38,15 @@ def home(request):
 def recipes(request, dish):
     # Получаем коэффициент для умножения количества каждого ингредиента в блюде (по-умолчанию 1)
     num = int(request.GET.get('servings', 1))
-    # Получаем блюдо из словаря DATA
-    dish_only = DATA[dish]
-    for key, value in dish_only.items():
+    # Получаем блюдо (словарь ингредиенты:количество) из словаря DATA
+    dish_ingredients = DATA[dish]
+    # Создаем новый словарь (ингредиенты:количество) данного блюда, чтобы не записывать изменения в словарь DATA
+    dish_ingredients = {key:value for key,value in dish_ingredients.items()}
+    for key, value in dish_ingredients.items():
         # Умножаем коэффициент на количество каждого ингредиента в блюде (по-умолчанию 1)
-        dish_only[key] = round(value * num, 2)
+        dish_ingredients[key] = round(value * num, 2)
     data = {'title': 'Страница блюда',
             'dish': dish,
-            'recipe': dish_only,
+            'recipe': dish_ingredients,
             }
     return render(request, 'calculator/index.html', context=data)
